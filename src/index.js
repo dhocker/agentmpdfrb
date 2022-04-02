3a0225ad589979281c21192d8406d5a626a04143
Dave Hocker <AtHomeX10@gmail.com>
2022-04-02 12:11:42 -0500

    Upgrade from React 17 to React 18

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@
 */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
@@ -26,11 +26,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 // components takes precedence over default styles.
 import './bootstrap-custom.scss';
 
-ReactDOM.render(
+// React 18
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
--- version.py
+++ version.py
@@ -22,7 +22,7 @@ def GetVersion():
   """
   Returns the current app version
   """
-  return "2022.0.1.0"
+  return "2022.0.2.0"
 
 def GetMPDVersion():
     player = Player()
